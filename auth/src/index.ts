76074ab08bfaf41a97eab5c633d53f4dc904b9a5
tavolafourcade <tavolafourcade@gmail.com>
2023-06-24 22:23:46 -0500

    [ ADD ] Dummy comment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(json());
 
 app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+  res.send('Hello there!');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
